a3c58424b251cef61819eea54872507e15034df8
NADIN <mashaicq@mail.ru>
2021-07-09 11:27:38 +0300

    lesson1. Introduction to ReactJS. The first components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class MessageClass extends React.Component {
+  render () {
+    return <p className="MessageClass_border">MessageClass - {this.props.text} <br />{this.props.text1}</p>    
+  }
+}
+
+function Message (props) {
+  console.log({ props })
+  return <p className="Message_border">Message - {props.text} <br />{props.text1}</p>  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">   
+      <img src={logo} className="App-logo" alt="logo" />      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Здравствуйте, Мария! <br /> Практическое задание к уроку 1.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message text={"Применяю функциональный компонент!"} text1={"Добавляю text1"}/>
+        <MessageClass text={"Применяю классовый компонент!"} text1={"Добавляю text1"}/>
       </header>
     </div>
   );
